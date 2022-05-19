94ea0259afc5cdcdfe7a5878ff2cadfa25997d05
fracabu <100199965+fracabu@users.noreply.github.com>
2022-05-19 23:53:02 +0200

    mod

---
+++ js/script.js
@@ -0,0 +1,51 @@
+
+
+const eta = prompt("inserisci la tua età")
+const kmDaPercorrere = prompt("quanti chilometri devi percorrere?")
+
+
+
+
+
+// formula calcolo prezzo 
+const prezzoAlkm = 0.21;
+let prezzoFinale = parseInt(km) * prezzoAlkm;
+console.log("prezzo originale:", prezzoFinale)
+
+
+if(parseInt(eta) < 18) {
+    prezzoFinale = prezzoFinale - prezzoFinale * 0.2;
+} else if(parseInt(eta) > 65){
+    prezzoFinale = prezzoFinale - prezzoFinale * 0.4;
+}
+
+console.log("prezzo scontato:", prezzoFinale)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const generaElement = document.getElementById('genera');
+//const annullaElement = document.getElementById('annulla');
+//const nomeElement = document.getElementById('nome');
+//const kmElement = document.getElementById('km');
+//const etaElement = document.getElementById('eta');
+
+
+
+//generaElement.innerHTML += generaElement;
+//annullaElement.innerHTML += annullaElement;
+//nomeElement.innerHTML += nomeElement;
+//kmElement.innerHTML +=  kmElement;
+//etaElement.innerHTML += etaElement;
+
+const textWhiteElement = document.querySelector('.text-white');
